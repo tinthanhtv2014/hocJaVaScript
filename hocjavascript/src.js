7b0d9c25740c3e719c2f52bb93da463778b1e08f
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2024-01-12 21:33:37 +0700

    bài 3

--- hocjavascript/src.js
+++ hocjavascript/src.js
@@ -1,6 +1,15 @@
+// bài 1
 console.log("hello world!");
 alert("hello world!");
 
+//bài 2
 let a = 5.5;
 const x = 5;
 console.log(`hello world! with 'hello world' with"hello world"`, x);
+
+//bài 3
+let test = "i am string";
+console.log("hello world with", test);
+
+console.log(5 + 4 + "a");
+console.log("a " + 5 + 4);
