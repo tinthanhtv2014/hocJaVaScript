2c0bd5a10fd4d0c5567bd01cbcc1d1e4dbbcd12c
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2024-01-12 22:05:36 +0700

    bài 6

--- hocjavascript/src.js
+++ hocjavascript/src.js
@@ -17,3 +17,16 @@ console.log("a " + 5 + 4);
 //bài 4
 let newtest = test.slice(0, 5);
 console.log("hello world with", newtest);
+
+// bài 6
+console.log("hello world from string".toLocaleUpperCase());
+let b = 5;
+let c = 6;
+let d = 6.9;
+let e = "5";
+console.log(a + b);
+console.log(c + d);
+console.log(b === e);
+console.log(b - e);
+console.log(b * e);
+console.log(b / e);
